65ba09612c5d099f3b0faba31572acef0fe7aee4
y-shino <sss.shinoshino1131@gmail.com>
2021-11-17 19:56:52 +0900

    commentの設定

--- board_laravel/app/Http/Controllers/ThreadController.php
+++ board_laravel/app/Http/Controllers/ThreadController.php
@@ -3,23 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Category;
+use App\Models\Comment;
+
 
 class ThreadController extends Controller
 {
-    /**
-     * スレッド一覧
-     * 
-     * @param Request $request
-     * @return Response
-     */
+    // Indexページの表示
     public function index(Request $request)
     {
         $comments = Category::find($request->id)->comments()->get(); 
       
 
-       return view('threads.index',['comments'=>$comments]);
+        return view('threads.index',[
+            'category_id'=>$request->id,
+            'comments'=>$comments
+        ]);
+    }
+
+    // 投稿された内容を表示するページ
+    public function create(Request $request)
+    {
+        // バリデーションチェック
+        $request->validate([
+            'name' => 'required|max:10',
+            'comment' => 'required|min:1|max:100',
+        ]);
+        // 投稿内容を受け取って変数に入れる
+        $name = $request->input('name');
+        $comment = $request->input('comment');
+
+        Comment::create([
+            'user_id' => 1,
+            'category_id' => $request->category_id,
+            'comment' => $comment
+        ]);
+
+        return redirect()->route('threads', ['id' => $request->category_id]);
     }
 }
 
--- board_laravel/routes/web.php
+++ board_laravel/routes/web.php
@@ -24,5 +24,5 @@ Route::get('/category', [App\Http\Controllers\CategoryController::class, 'catego
 Route::post('/category', [App\Http\Controllers\CategoryController::class, 'category'])->name('category');
 Route::get('/category-add', [App\Http\Controllers\CategoryController::class, 'category_add'])->name('category-add');
 Route::post('/add', [App\Http\Controllers\CategoryController::class, 'add'])->name('add');
-Route::get('/threads/{id}', [App\Http\Controllers\ThreadController::class, 'index'])->name('threads/{id}');
-
+Route::get('/threads/{id}', [App\Http\Controllers\ThreadController::class, 'index'])->name('threads');
+Route::post('/threads', [App\Http\Controllers\ThreadController::class,'create']);
